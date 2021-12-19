5c3a17a82565aeedf6784b1a7ad9ad5d6ddecbc4
xweronika <xweronika19@gmail.com>
2021-12-19 20:01:57 +0100

    update

--- ngApp/src/app/core/plugins/material/material.module.ts
+++ ngApp/src/app/core/plugins/material/material.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 
 @NgModule({
