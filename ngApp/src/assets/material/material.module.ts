e87da95e28e0a9da31f1a2621012945ff5b66dda
xweronika <xweronika19@gmail.com>
2022-01-06 17:47:21 +0100

    update

--- ngApp/src/assets/material/material.module.ts
+++ ngApp/src/assets/material/material.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { MatSelectModule } from '@angular/material/select';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   exports: [
     MatSelectModule,
-    MatToolbarModule,
-    MatButtonModule
+    MatToolbarModule
   ]
 })
 export class MaterialModule { }
