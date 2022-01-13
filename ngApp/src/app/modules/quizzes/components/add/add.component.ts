122ca5ce276ffd7ee36aeed5fb2967dadb560720
xweronika <xweronika19@gmail.com>
2022-01-13 12:29:41 +0100

    update form

--- ngApp/src/app/modules/quizzes/components/add/add.component.ts
+++ ngApp/src/app/modules/quizzes/components/add/add.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, FormArray, Validators, FormControl, ValidationErrors } from '@angular/forms';
+import { FormGroup, FormBuilder, FormArray, Validators, FormControl } from '@angular/forms';
 import { Details } from '../../../../core/interfaces/details'
 
 @Component({
@@ -9,16 +9,10 @@ import { Details } from '../../../../core/interfaces/details'
 })
 export class AddComponent implements OnInit {
   public form: FormGroup;
-  public category = new FormControl('', [
-    Validators.required,
-    Validators.minLength(2),
-    Validators.maxLength(50)
-  ]);
-  public description = new FormControl('', [
-    Validators.required,
-    Validators.minLength(5),
-    Validators.maxLength(50)
-  ]);
+  public category = this.newForm(2, 50);
+  public description = this.newForm(10, 100);
+  public question = Array.from({ length: 20 }, () => this.newForm(10, 70));
+  public answer = Array.from({ length: 20 }, () => this.newForm(1, 50));
 
   constructor(private fb: FormBuilder) {
     this.form = this.fb.group({
@@ -27,21 +21,25 @@ export class AddComponent implements OnInit {
       details: this.fb.array([])
     });
     this.addQuestion();
+    this.addQuestion();
   }
   ngOnInit(): void { }
 
   onSubmit() {
-    this.setCorrect();
+    this.form.controls['details'].value.forEach((el: Details) => {
+      el.correct = el.answers[parseInt(el.correct)];
+    });
     console.log(this.form.controls['details'] as FormArray)
     console.log(this.form)
   }
 
   addQuestion() {
     const details = this.form.controls['details'] as FormArray;
+    const answers = this.answer.slice(details.length * 4, details.length * 4 + 4)
     if (details.length < 20) details.push(this.fb.group({
-      question: '',
+      question: this.question[details.length],
       correct: 0,
-      answers: this.fb.array(['', '', '', '']),
+      answers: this.fb.array(answers),
     }));
   }
 
@@ -54,26 +52,23 @@ export class AddComponent implements OnInit {
     return index;
   }
 
-  numToStr(num1: number, num2: number) {
-    return num1.toString() + num2.toString()
-  }
-
   radioChange(detailIndex: number, radioIndex: number) {
     this.form.controls['details'].value[detailIndex].correct = radioIndex;
   }
 
-  setCorrect() {
-    this.form.controls['details'].value.forEach((el: Details) => {
-      el.correct = el.answers[parseInt(el.correct)];
-    });
-  }
-  getError(value: FormControl | FormArray, name: string, min?: number, max?: number) {
+  getError(value: FormControl | FormArray, name: string, min: number, max: number) {
     if (value.hasError('required')) return `${name} is required.`;
-    else if (value.hasError('minlength')) return `${name} can not be shorter than ${min || 5} characters.`;
-    else if (value.hasError('maxlength')) return `${name} can not be longer than ${max || 25} characters.`;
+    else if (value.hasError('minlength')) return `${name} can not be shorter than ${min} characters.`;
+    else if (value.hasError('maxlength')) return `${name} can not be longer than ${max} characters.`;
     return "";
   }
 
-
+  newForm(min: number, max: number) {
+    return new FormControl('', [
+      Validators.required,
+      Validators.minLength(min),
+      Validators.maxLength(max)
+    ])
+  }
 
 }
